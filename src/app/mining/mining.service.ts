cacc606e30aa06d760e458d8c2881bb3a2390d69
Nathaniel Lord <nathaniel.lord@gmail.com>
2018-01-09 16:04:48 -0700

    Display miners and update on a 10 second interval

--- src/app/mining/mining.service.ts
+++ src/app/mining/mining.service.ts
@@ -3,17 +3,30 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+interface MinerResponse {
+  miners: Miner[]
+}
+interface Miner {
+  name: string;
+  difficulty: number;
+  hashrate: string;
+  lastaccepted: string;
+  pool: string;
+  gpus: string[];
+  rejected: number;
+  threads: string;
+}
+
 @Injectable()
 export class MiningService {
 
   constructor(private http: HttpClient) { }
 
-  getMiners() {
-    return this.http.get('http://localhost:3050/mining');
+  getMiners(): Observable<Miner[]> {
+    return this.http.get('http://localhost:3050/mining').map((response: MinerResponse) => response.miners);
   }
 
   startMining(poolid, gpus): Observable<Object> {
-    console.log('Starting mining!!');
     return this.http.post('http://localhost:3050/mining/miner', { poolid: poolid, gpus: gpus });
   }
 
--- src/app/mining/mining/mining.component.ts
+++ src/app/mining/mining/mining.component.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TitleService } from '../../shared/title.service';
 import { MatDialog } from '@angular/material';
 import { MiningModalComponent } from '../mining-modal/mining-modal.component';
+import { MiningService } from '../mining.service';
 
 @Component({
   selector: 'app-mining',
   templateUrl: './mining.component.html',
   styleUrls: ['./mining.component.scss']
 })
-export class MiningComponent implements OnInit {
+export class MiningComponent implements OnInit, OnDestroy {
 
-  constructor(private titleService: TitleService, public dialog: MatDialog) {
+  miners;
+  stats = {
+    hashrate: 0,
+    miners: 0,
+    activeGPUs: 0
+  };
+  interval;
+
+  constructor(private titleService: TitleService,
+    public dialog: MatDialog,
+    private miningService: MiningService) {
     titleService.setTitle('Mining');
   }
 
   ngOnInit() {
+    this.getMiners();
+    this.interval = setInterval(() => { this.getMiners(); }, 10000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  getMiners() {
+    this.miningService.getMiners().subscribe(miners => {
+      this.miners = miners;
+      this.processStats();
+    });
+  }
+
+  processStats() {
+    this.stats.hashrate = this.miners.map(value => parseFloat(value.hashrate)).reduce((previousValue, currentValue) => previousValue + currentValue);
+    this.stats.miners = this.miners.length;
+    this.stats.activeGPUs = this.miners.map(value => value.gpus.length).reduce((previousValue, currentValue) => previousValue + currentValue);
   }
 
   addMiner() {
